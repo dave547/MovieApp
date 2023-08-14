952aa7524251b5dab7f41c3ae9c14d3cb0cd64fd
Dave Tyler <dave@davetyler.net>
2023-08-13 22:47:31 -0600

    flushed out api for listing movies

--- src/movies/MoviesList.js
+++ src/movies/MoviesList.js
@@ -20,8 +20,7 @@ import { Filter } from "../Filter";
 //     }
 // ]
 
-const API_URL = "https://api.themoviedb.org/3/discover/movie?api_key=6d3c7fa4bf49bd928d321b9b36e71081&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1";
-const CONFIG_URL = "https://api.themoviedb.org/3/configuration?api_key=6d3c7fa4bf49bd928d321b9b36e71081";
+// add api stuff back
 
 export function MoviesList() {
     const [filter, setFilter] = useState("")
